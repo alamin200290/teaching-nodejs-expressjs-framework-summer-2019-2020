f739a7737255c752b67fab0ea016ace64ff58780
sarwar_sikder <amin.se.aiub@gmail.com>
2020-07-14 11:37:16 +0600

    ajax+jquery+json example

--- jquery/abc.php
+++ jquery/abc.php
@@ -1,5 +1,10 @@
 <?php
 
-	$obj = json_decode($_REQUEST['json']);
-	echo $obj->id;
+	//$obj = json_decode($_REQUEST['json']);
+	//echo $obj->id;
+
+	$data = ['name'=>'alamin','id'=>'12'];
+
+	//header('Content-type:application/json');
+	echo json_encode($data);
 ?>
