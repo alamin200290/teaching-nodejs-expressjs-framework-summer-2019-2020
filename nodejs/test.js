75654112c2b967264c06310fbab70c591755990c
sarwar_sikder <amin.se.aiub@gmail.com>
2020-07-14 13:38:49 +0600

    node hello world console program

---
+++ nodejs/test.js
@@ -0,0 +1,21 @@
+//console.log('hello from nodejs');
+
+setTimeout(function(){
+	console.log('5s..');
+}, 5000);
+
+setTimeout(function(){
+	console.log('3s...');
+}, 3000);
+
+setTimeout(function(){
+	console.log('6s...');
+}, 6000);
+
+setTimeout(function(){
+	console.log('2s...');
+}, 2000);
+
+setTimeout(function(){
+	console.log('3s...');
+}, 3000);
